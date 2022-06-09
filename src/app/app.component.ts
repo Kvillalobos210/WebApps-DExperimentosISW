da445f6973737526f2200551600a57f92ba721e9
Juben Cacha <u201815446@upc.edu.pe>
2022-06-09 03:59:25 -0500

    Google Analytics Integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import {environment} from '../environments/environment';
+
+declare let gtag: (property: string, value: any, configs: any) => {};
 
 @Component({
   selector: 'app-root',
@@ -7,6 +11,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Frontend';
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', environment.googleAnalyticsId, {
+          page_path: event.urlAfterRedirects
+        });
+      }
+    });
+  }
 
 }
 
